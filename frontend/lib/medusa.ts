a3f875d1724bfa73f71d6d1ced124d0db4f65489
Mansha Soni <manshasoni23@gmail.com>
2026-03-11 13:20:03 +0530

    Cleanup submodules

--- frontend/lib/medusa.ts
+++
@@ -1,7 +0,0 @@
-import Medusa from "@medusajs/js-sdk"
-
-export const medusa = new Medusa({
-  baseUrl: process.env.NEXT_PUBLIC_MEDUSA_URL || "http://localhost:9000",
-  debug: process.env.NODE_ENV === "development",
-  publishableKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || "pk_test_123",
-})
